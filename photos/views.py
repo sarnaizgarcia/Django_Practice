c255e69e2366b216c360a2da626d2d0d14880cc3
Silvia <sarnaizgarcia@gmail.com>
2019-06-25 12:23:49 +0200

    Solución error del commit anterior (method object is not subscriptable)

--- photos/views.py
+++ photos/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 
 def hello(request):
-    nombre = request.GET.get['nombre']
-    apellido = request.GET.get['apellido']
-    return HttpResponse('Hello {0}, {1}'.format(nombre, apellido))
+    nombre = request.GET.get('nombre')
+    apellido = request.GET.get('apellido')
+    return HttpResponse('Hello {0} {1}'.format(nombre, apellido))
