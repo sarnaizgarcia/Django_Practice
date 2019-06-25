2bc5cfef4ce9635ef6e89c7aed609618efedd73d
Silvia <sarnaizgarcia@gmail.com>
2019-06-25 11:31:50 +0200

    Hello world en Django

--- frikr/urls.py
+++ frikr/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from photos.views import hello
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello),
 ]
