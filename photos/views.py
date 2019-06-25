4c687fd5639c964ca1c72889eac0ec24bf36adb9
Silvia <sarnaizgarcia@gmail.com>
2019-06-25 12:05:57 +0200

    Creada primera vista y prueba de los parámetros GET de las peticiones HTTP

--- photos/views.py
+++ photos/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 
 def hello(request):
-    return HttpResponse('Hello world!')
+    nombre = request.GET.get['nombre']
+    apellido = request.GET.get['apellido']
+    return HttpResponse('Hello {0}, {1}'.format(nombre, apellido))
