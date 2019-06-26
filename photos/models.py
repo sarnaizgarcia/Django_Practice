c59bc83f7fff0b6fd8b5becf2cdf0627a60429da
Silvia <sarnaizgarcia@gmail.com>
2019-06-26 11:33:22 +0200

    Creado el modelo de Photo

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Photo(models.Model):
+
+    LICENSES = (
+        ('RIG', 'Copyright'),
+        ('LEF', 'Copyleft'),
+        ('CC', 'Creative commons')
+    )
+
+    name = models.CharField(max_length=150)
+    url = models.URLField()
+    description = models.TextField()
+    license = models.CharField(max_length=3, choices=LICENSES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
