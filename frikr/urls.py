336102ff18415832e067ff0e618047ddc9d208f0
Silvia <sarnaizgarcia@gmail.com>
2019-06-26 13:12:07 +0200

    Creada plantilla con listado de fotos de la home

--- frikr/urls.py
+++ frikr/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from photos.views import hello
+from photos.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', hello),
+    path('', home),
 ]
--- photos/views.py
+++ photos/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from photos.models import Photo
 
-
-def hello(request):
-    nombre = request.GET.get('nombre')
-    apellido = request.GET.get('apellido')
-    return HttpResponse('Hello {0} {1}'.format(nombre, apellido))
+def home(request):
+    '''
+    Renderiza el home con un listado de fotos
+    ;param request: objeto HttpRequest con los datos de la petición
+    ;return: objeto HttpResponse con los datos de la respuesta
+    '''
+    photos = Photo.objects.all()  # recupera todas las fotos de la base de datos
+    context = {'photos_list': photos}
+    return render(request, 'photos/home.html', context)
